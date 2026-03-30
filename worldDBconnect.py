c82fc467162edf3ceeb42c78cc8e049e8e328f24
swinnen.noah <swinnen.noah@provilion.be>
2026-03-30 14:31:35 +0200

    exc2 vervolg

--- worldDBconnect.py
+++ worldDBconnect.py
@@ -1,4 +1,10 @@
 import mysql.connector
+
+#2.1 start import tkinter
+import tkinter
+
+#2.1 end
+
 #1 start connect
 DB_CONFIG = {
     'host': "Localhost",
@@ -6,12 +12,27 @@ DB_CONFIG = {
     'password': "IkBenZeerKazig02062009!",
     'database': "world"
 }
-
-conn = mysql.connector.connect(**DB_CONFIG)
-if conn.is_connected():
-    print('good boy')
-else:
-    print('bad boy')
+def get_connection():
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor()
+    return conn, cursor
 #1 end start connect
 
-#2 start Landenweergeven in Tkinter
+#2.2 start tkinter venster
+window = tkinter.Tk()
+#2.2 end
+
+listb_landen = tkinter.Listbox(window)
+listb_landen.pack(padx=100, pady=100)
+
+#2.3 listbox add alle landen
+def voeg_landen_toe():
+    conn, cursor = get_connection()
+    query = 'select * from Country;'
+    cursor.execute(query)
+    results = cursor.fetchall()
+    for i in results:
+        listb_landen.insert(tkinter.END, str(i))
+
+voeg_landen_toe()
+window.mainloop()
